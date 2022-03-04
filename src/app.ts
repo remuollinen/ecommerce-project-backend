bb53883ab8e5dd3efc0b655377f5f5c7502d3e0f
Remu Ollinen <remu.ollinen@gmail.com>
2022-03-04 15:23:08 +0200

    trying post method with cart items

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import products from "./products.json";
+import cart from "./cart.json";
 import cors from "cors";
 
 const app: Application = express();
 
 app.use(cors());
+app.use(express.json());
+
 app.get("/api/products", (req: Request, res: Response) => {
 	try {
 		res.send(products);
@@ -24,4 +27,19 @@ app.get("/api/products/:id", (req: Request, res: Response) => {
 	}
 });
 
+app.get("/api/cart", (req: Request, res: Response) => {
+	try {
+		res.send(cart);
+	} catch (error) {
+		console.log(error);
+	}
+});
+app.post("/api/cart", (req: Request, res: Response) => {
+	try {
+		cart.push(req.body);
+		res.send(cart);
+	} catch (error) {
+		console.log(error);
+	}
+});
 app.listen(4000, () => console.log("server running"));
