64e435abd0807aefcb75bbb5adacebf689a5c6d0
Ouss84 <bahrioussama84@gmail.com>
2022-03-15 15:07:06 +0200

    fixing addToCart bug

--- src/app.ts
+++ src/app.ts
@@ -49,15 +49,15 @@ app.post("/api/cart", (req: Request, res: Response) => {
     const product = { ...req.body, quantity: 1 };
     const productExist = cartItems.find((item) => item.id === product.id);
 
-    if (cartItems.length > 1 || productExist) {
+    if (cartItems.length < 1 || !productExist) {
+      cartItems.push(product);
+    } else {
       for (let item of cartItems) {
         if (item.id === product.id) {
           item.quantity++;
           break;
         }
       }
-    } else {
-      cartItems.push(product);
     }
     res.json(cartItems);
   } catch (error) {
